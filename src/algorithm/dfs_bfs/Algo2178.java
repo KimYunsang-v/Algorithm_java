6c85e9d32dd7e233311d029f4183ed4ed5ee447c
kimYSang <qwdbstkd@naver.com>
2018-11-05 10:44:38 +0900

    백준 2178번

--- src/algorithm/dfs_bfs/Algo2178.java
+++ src/algorithm/dfs_bfs/Algo2178.java
@@ -1,8 +1,71 @@
 package algorithm.dfs_bfs;
 
-public class Algo2178 {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class Algo2178 {
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static int N, M;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        sc.nextLine();
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            String str = sc.nextLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+                visited[i][j] = false;
+            }
+        }
+        visited[0][0] = true;
+        BFS(0, 0);
+        System.out.println(arr[N - 1][M - 1]);
     }
 
+    static public void BFS(int x, int y) {
+        Queue<Dot> q = new LinkedList<Dot>();
+        q.add(new Dot(x, y));
+
+        while (!q.isEmpty()) {
+            Dot d = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nextX = d.x + dx[i];
+                int nextY = d.y + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
+                    continue;
+                }
+
+                if (visited[nextX][nextY] || arr[nextX][nextY] == 0) {
+                    continue;
+                }
+
+                q.add(new Dot(nextX, nextY));
+
+                arr[nextX][nextY] = arr[d.x][d.y] + 1;
+
+                visited[nextX][nextY] = true;
+            }
+        }
+    }
+}
+
+class Dot {
+    int x;
+    int y;
+
+    Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
